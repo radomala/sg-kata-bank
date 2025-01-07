3a2d42b3343481199aec5772f53620aad1bcf952
radomala <radomala@gmail.com>
2025-01-07 23:49:15 +0100

    us 2 : In order to retrieve some or all of my savings as a bank client I want to make a withdrawal from my account

--- src/main/java/org/sg/bankaccount/BankAccount.java
+++ src/main/java/org/sg/bankaccount/BankAccount.java
@@ -2,23 +2,22 @@ package org.sg.bankaccount;
 
 public class BankAccount {
 
-    private double balance;
+    private double solde = 0.0;
 
-    public double getBalance() {
-        return balance;
-    }
-
-    public BankAccount() {
-        this.balance = 0.0; // Initialisation du solde à 0
+    public double getSolde() {
+        return solde;
     }
 
     public void deposit(double amount) {
-        balance += (amount > 0) ? amount : throwException("Your deposit is invalid, please enter a value greater than zero");
+        solde += (amount > 0) ? amount : throwException("Your deposit is invalid, please enter a value greater than zero");
+    }
+
+    public void withdraw(double amount) {
+        solde = (amount > 0 && amount <= solde) ? solde - amount : throwException ("Invalid withdrawal amount");
     }
 
     private double throwException(String message) {
         throw new IllegalArgumentException(message);
     }
-
 
 }
